8f5c1cb6091d3ff38380c0e41b4c836b8ced67c9
tscriv2799 <tscriv@bu.edu>
2019-12-10 20:24:36 -0500

    I added more actvities to our app and implemented the button view feature and image view feature for some activities

--- app/src/main/java/com/example/homepage/Page2.java
+++ app/src/main/java/com/example/homepage/Page2.java
@@ -1,5 +1,6 @@
 package com.example.homepage;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,8 +10,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class Page2 extends AppCompatActivity {
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,38 @@ public class Page2 extends AppCompatActivity {
         setContentView(R.layout.activity_page2);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        button1 = findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPage3();
+            }
+        });
+
+        button2 = findViewById(R.id.button3);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPage4();
+            }
+        });
+
+        button3 = findViewById(R.id.button4);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPage5();
+            }
+        });
+
+        button4 = findViewById(R.id.button5);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPage6();
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +65,26 @@ public class Page2 extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void openPage3(){
+        Intent intent = new Intent(this, Page3.class);
+        startActivity(intent);
+    }
+
+    public void openPage4(){
+        Intent intent = new Intent(this, Page4.class);
+        startActivity(intent);
+    }
+
+    public void openPage5(){
+        Intent intent = new Intent(this, Page5.class);
+        startActivity(intent);
+    }
+
+    public void openPage6(){
+        Intent intent = new Intent(this, Page6.class);
+        startActivity(intent);
     }
 
 }
